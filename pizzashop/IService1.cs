5265eb06b7ad59020ec58e3dd5bd3c153c37803a
IST420 <eot5104@gmail.com>
2016-04-27 21:13:45 -0400

    Added a whole lot of stuff

---
+++ pizzashop/IService1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace pizzashop
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        //[OperationContract]
+        //void DoWork();
+
+        [WebGet(UriTemplate = "/Create?FoodName={FoodN}&Price={FoodPrice}&Quantity={FoodQ}",
+            ResponseFormat = WebMessageFormat.Json)]
+        [OperationContract]
+        string CreateFood(string FoodN, string FoodPrice, string FoodQ, string email);
+
+        //[WebGet(UriTemplate = "/Update?FoodName={FoodN}&Price={FoodPrice}&Quantity={FoodQ}",
+        //    ResponseFormat =WebMessageFormat.Json)]
+        //[OperationContract]
+        //string UpdateFood(string FoodN, string FoodPrice, string FoodQ);
+
+        //[WebGet(UriTemplate = "/Read?FoodName={FoodN}&Price={FoodPrice}&Quantity={FoodQ}",
+        //    ResponseFormat =WebMessageFormat.Json)]
+        //[OperationContract]
+        //string ReadFood(string FoodN, string FoodPrice, string FoodQ);
+
+        //[WebGet(UriTemplate = "/Delete?FoodName={FoodN}&Price={FoodPrice}&Quantity={FoodQ}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string DeleteFood(string FoodN, string FoodPrice, string FoodQ);
+
+
+
+        //[WebGet(UriTemplate = "/Create?CusFirstName={CusFN}&CusLastName={CusLN}&CusPhoneNum={CusPN}&CusAddress={CusAdd}&CusOrder={CusO}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string CreateCustomer(string CusFN, string CusLN, string CusPN, string CusAdd, string CusO);
+
+        //[WebGet(UriTemplate = "/Read?CusFirstName={CusFN}&CusLastName={CusLN}&CusPhoneNum={CusPN}&CusAddress={CusAdd}&CusOrder={CusO}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string ReadCustomer(string CusFN, string CusLN, string CusPN, string CusAdd, string CusO);
+
+        //[WebGet(UriTemplate = "/Update?CusFirstName={CusFN}&CusLastName={CusLN}&CusPhoneNum={CusPN}&CusAddress={CusAdd}&CusOrder={CusO}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string UpdateCustomer(string CusFN, string CusLN, string CusPN, string CusAdd, string CusO);
+
+        //[WebGet(UriTemplate = "/Delete?CusFirstName={CusFN}&CusLastName={CusLN}&CusPhoneNum={CusPN}&CusAddress={CusAdd}&CusOrder={CusO}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string DeleteCustomer(string CusFN, string CusLN, string CusPN, string CusAdd, string CusO);
+
+
+        //[WebGet(UriTemplate = "/Create?ID={WID}&FirstName={WFN}&LastName={WLN}&Address={WADD}&PhoneNum={WNUM}&UserName={WUSER}&PassWord{WPW}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string CreateWorker(string WFN, string WLN, string WADD, string WNUM, string WUSER, string WPW);
+
+        //[WebGet(UriTemplate = "/Read?ID={WID}&FirstName={WFN}&LastName={WLN}&Address={WADD}&PhoneNum={WNUM}&UserName={WUSER}&PassWord{WPW}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string ReadWorker(string WFN, string WLN, string WADD, string WNUM, string WUSER, string WPW);
+
+        //[WebGet(UriTemplate = "/Update?ID={WID}&FirstName={WFN}&LastName={WLN}&Address={WADD}&PhoneNum={WNUM}&UserName={WUSER}&PassWord{WPW}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string UpdateWorker(string WFN, string WLN, string WADD, string WNUM, string WUSER, string WPW);
+
+        //[WebGet(UriTemplate = "/Delete?ID={WID}&FirstName={WFN}&LastName={WLN}&Address={WADD}&PhoneNum={WNUM}&UserName={WUSER}&PassWord{WPW}",
+        //    ResponseFormat = WebMessageFormat.Json)]
+        //[OperationContract]
+        //string DeleteWorker(string WFN, string WLN, string WADD, string WNUM, string WUSER, string WPW);
+
+
+
+
+    }
+}
---
+++ pizzashop/Service1.svc.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Net.Mail;
+
+namespace pizzashop
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public void DoWork()
+        {
+
+        }
+        public string CreateFood(string FoodN, string FoodPrice, string FoodQ, string email)
+        {
+            using (ist420row1Entities entities = new ist420row1Entities())
+            {
+                Food FD = new Food();            
+                FD.FoodName = FoodN;
+                FD.Price = Convert.ToInt32(FoodPrice);
+                FD.Quantity = Convert.ToInt32(FoodQ);
+                entities.Foods.Add(FD);
+                entities.SaveChanges();
+
+                if (FD.FoodId > 0)
+                    return FD.FoodId.ToString();
+                else
+                    return "false";
+
+
+
+            }
+        }
+        //    public bool UpdateCreateFood(string FoodN, string FoodPrice, string FoodQ)
+        //    {
+        //        using (ist420row1Entities entities = new ist420row1Entities())
+        //        {
+        //            Food FD = new Food();
+        //            FD = entities.Foods.Find(FoodN);
+        //            FD.FoodName = (FoodN == null) ? FD.FoodName : FoodN;
+        //            FD.Price = (Convert.ToInt32(FoodPrice) == 0) ? FD.Price : Convert.ToInt32(FoodPrice);
+        //            FD.Quantity = (Convert.ToInt32(FoodQ) == 0) ? FD.Quantity : Convert.ToInt32(FoodQ);
+
+        //            entities.SaveChanges();
+        //            if (FD.FoodId > 0)
+        //                return true;
+        //            else
+        //                return false;
+        //        }
+        //    }
+        //    public FD ReadFood()
+        //}
+    }
+}
